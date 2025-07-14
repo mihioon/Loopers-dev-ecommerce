11088ca6b3be6bc2b58a004ed4ced078b62c003a
mihioon <rangee3014@gmail.com>
2025-07-14 23:53:22 +0900

    chore: User 도메인 구조 빌딩

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/UserService.java
@@ -0,0 +1,9 @@
+package com.loopers.domain.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class UserService {
+}
---
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1ApiController.java
@@ -0,0 +1,11 @@
+package com.loopers.interfaces.api.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserV1ApiController {
+}
