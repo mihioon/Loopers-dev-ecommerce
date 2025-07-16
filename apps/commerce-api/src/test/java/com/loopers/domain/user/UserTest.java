29d4838f3e9e7f5be3f5426b969379737af25c45
mihioon <rangee3014@gmail.com>
2025-07-16 23:33:55 +0900

    test: 사용자 회원가입 기능 테스트 추가

--- apps/commerce-api/src/test/java/com/loopers/domain/user/UserTest.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/user/UserTest.java
@@ -1,10 +1,12 @@
 package com.loopers.domain.user;
 
 import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserTest {
@@ -19,23 +21,27 @@ public class UserTest {
             "test",
             "testTentex",
             "test1234567890",
+            "test1234567",
             "1234567890",
-            ""
+            "test____",
+            "",
+            "1",
+            "A"
     })
-    void fail_whenIdFormatIsInvalid(String userId) {
-        // arrange
+    void fail_whenIdFormatIsInvalid(String loginId) {
+        // given
+        final String name = "test";
+        final String gender = "F";
+        final String email = "test@example.com";
+        final String dob = "2025-01-01";
 
-        // act
+        // when
         final CoreException exception = assertThrows(CoreException.class, () -> {
-            UserEntity exampleModel = UserEntity.create(
-                    userId,
-                    "test",
-                    "test@example.com",
-                    "2025-01-01"
-            );
+            UserEntity userEntity = UserEntity.create(loginId, name, gender, email, dob);
         });
 
-        // assert
+        // then
+        assertThat(exception.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
     }
 
     @DisplayName("이메일이 `xx@yy.zz` 형식에 맞지 않으면, User 객체 생성에 실패한다.")
@@ -64,19 +70,19 @@ public class UserTest {
             ""
     })
     void fail_whenEmailFormatIsInvalid(String email) {
-        // arrange
+        // given
+        final String loginId = "test123456";
+        final String gender = "F";
+        final String name = "test";
+        final String dob = "2025-01-01";
 
-        // act
+        // when
         final CoreException exception = assertThrows(CoreException.class, () -> {
-            UserEntity exampleModel = UserEntity.create(
-                    "test123456",
-                    "test",
-                    email,
-                    "2025-01-01"
-            );
+            UserEntity userEntity = UserEntity.create(loginId, name, gender, email, dob);
         });
 
-        // assert
+        // then
+        assertThat(exception.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
     }
 
     @DisplayName("생년월일이 `yyyy-MM-dd` 형식에 맞지 않으면, User 객체 생성에 실패한다.")
@@ -93,19 +99,19 @@ public class UserTest {
             ""
     })
     void fail_whenDobFormatIsInvalid(String dob) {
-        // arrange
+        // given
+        final String loginId = "test123456";
+        final String gender = "F";
+        final String name = "test";
+        final String email = "test@example.com";
 
-        // act
+        // when
         final CoreException exception = assertThrows(CoreException.class, () -> {
-            UserEntity exampleModel = UserEntity.create(
-                    "test123456",
-                    "test",
-                    "test@example.com",
-                    dob
-            );
+            UserEntity userEntity = UserEntity.create(loginId, name, gender, email, dob);
         });
 
-        // assert
+        // then
+        assertThat(exception.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
     }
 
 }
